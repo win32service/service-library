0e1e46d2203f1e8aa93b2566493cea1f1e82824a
Macintoshplus <jean-baptiste@nahan.fr>
2018-12-10 21:15:09 +0100

    fix define constants

--- lib/pollyfill.php
+++ lib/pollyfill.php
@@ -7,14 +7,14 @@
 /**
  * There constants are added in v0.4.0 of extension
  */
-if (!defined(WIN32_INFO_SERVICE)) {
-    define(WIN32_INFO_SERVICE, "service");
-    define(WIN32_INFO_DISPLAY, "display");
-    define(WIN32_INFO_USER, "user");
-    define(WIN32_INFO_PASSWORD, "password");
-    define(WIN32_INFO_PATH, "path");
-    define(WIN32_INFO_PARAMS, "params");
-    define(WIN32_INFO_DESCRIPTION, "description");
-    define(WIN32_INFO_START_TYPE, "start_type");
+if (!defined("WIN32_INFO_SERVICE")) {
+    define("WIN32_INFO_SERVICE", "service");
+    define("WIN32_INFO_DISPLAY", "display");
+    define("WIN32_INFO_USER", "user");
+    define("WIN32_INFO_PASSWORD", "password");
+    define("WIN32_INFO_PATH", "path");
+    define("WIN32_INFO_PARAMS", "params");
+    define("WIN32_INFO_DESCRIPTION", "description");
+    define("WIN32_INFO_START_TYPE", "start_type");
 }
 

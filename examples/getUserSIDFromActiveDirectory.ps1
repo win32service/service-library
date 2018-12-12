3dfb68f338ebaa6a14af986480fdd10c2c52d2ee
macintoshplus <jean-baptiste@nahan.fr>
2018-12-12 15:51:50 +0100

    clean old ps1

--- examples/getUserSIDFromActiveDirectory.ps1
+++
@@ -1,7 +0,0 @@
-$strComputer = "."
-
-$objWMIService = GetObject("winmgmts:\\$($strComputer)\root\cimv2")
-
-$objAccount = $objWMIService.Get _ ("Win32_UserAccount.Name='a_nahaje00',Domain='grpinextenso.dom'")
-
-Wscript.Echo $objAccount.SID

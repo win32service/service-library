4dd6b553b6b4449a403bf43432bc7bd791a043fd
macintoshplus <jean-baptiste@nahan.fr>
2018-12-13 14:01:57 +0100

    add admin test

---
+++ tests/Units/Service/ServiceAdminManager.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Win32Service Library package
+ * @copy Win32Service (c) 2018
+ * @author "Jean-Baptiste Nahan" <jean-baptiste@nahan.fr>
+ */
+
+namespace Win32Service\Tests\Units\Service;
+
+use atoum;
+use mock;
+
+/**
+ * Class ServiceStateManager
+ * @package Win32Service\Tests\Units\Service
+ * @extensions win32service
+ */
+class ServiceAdminManager extends atoum
+{
+    public function testRegistration()
+    {
+        $this->assert('start')
+            ->given($this->newTestedInstance())
+            ->if($this->function->win32_query_service_status = ['CurrentState'=>WIN32_SERVICE_STOPPED])
+            ->and($this->function->win32_create_service = WIN32_NO_ERROR)
+            ->then
+            ->variable($this->testedInstance->startService(\Win32Service\Model\ServiceIdentifier::identify('servideId')))->isNull
+        ;
+    }
+}

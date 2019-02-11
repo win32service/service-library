16b52e006e5d5589e82a62c1be0f6ac36d48010f
macintoshplus <jean-baptiste@nahan.fr>
2019-02-11 23:30:25 +0100

    add pollyfill and remove ext require

--- lib/pollyfill.php
+++ lib/pollyfill.php
@@ -18,3 +18,36 @@ if (!defined("WIN32_INFO_SERVICE")) {
     define("WIN32_INFO_START_TYPE", "start_type");
 }
 
+if (!function_exists('win32_start_service_ctrl_dispatcher')) {
+    function win32_start_service_ctrl_dispatcher($serviceName) {
+        return true;
+    }
+}
+
+if (!function_exists('win32_get_last_control_message')) {
+    function win32_get_last_control_message($serviceName) {
+        return WIN32_SERVICE_CONTROL_INTERROGATE;
+    }
+}
+
+if (!function_exists('win32_set_service_status')) {
+    function win32_set_service_status($status) {
+
+    }
+}
+if (!function_exists('win32_set_service_exit_mode')) {
+    function win32_set_service_exit_mode($GracefulExit) {
+
+    }
+}
+if (!function_exists('win32_set_service_exit_code')) {
+    function win32_set_service_exit_code($ExitCode) {
+
+    }
+}
+if (!function_exists('win32_send_custom_control')) {
+    function win32_send_custom_control($ServiceName, $Machine) {
+
+    }
+}
+

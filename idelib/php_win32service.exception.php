1bd9c918cd9f66d50e336370dc19d634243c6f5d
Fernando Herrero <alphp@users.noreply.github.com>
2024-03-13 17:04:54 +0100

    Define Win32ServiceException for IDE's (#8)

---
+++ idelib/php_win32service.exception.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/*
+ * php_win32service.dll provided exception class
+ * Author: Alphp
+ * Date: 3/13/24
+ * Version: 2.2
+ *
+ * Requirements: php_win32service.dll V1.0.x
+ */
+
+
+/*
+ * This file is NOT to be used in production!
+ * This file is for IDE's so they don't complain about unknown functions.
+ * Also allows IDE's to know how to use them. Made for Eclipse + PDT
+ * Your mileage may vary.
+ */
+
+class Win32ServiceException extends Exception {}
